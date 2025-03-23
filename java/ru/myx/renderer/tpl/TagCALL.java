8728ff9a7fd56261024a12a48e2c726e374b6755
Alex Kharichev <myx@myx.nz>
2025-03-24 01:19:39 +0200

    * minor: source (exception catch)

--- java/ru/myx/renderer/tpl/TagCALL.java
+++ java/ru/myx/renderer/tpl/TagCALL.java
@@ -1,8 +1,8 @@
 package ru.myx.renderer.tpl;
 
 import java.util.Stack;
-
 import java.util.function.Function;
+
 import ru.myx.ae3.act.Context;
 import ru.myx.ae3.base.BaseFunction;
 import ru.myx.ae3.eval.Evaluate;
@@ -15,28 +15,27 @@ import ru.myx.ae3.exec.ProgramPart;
 import ru.myx.ae3.exec.ResultHandler;
 import ru.myx.renderer.tpl.parse.Token;
 
-/**
- * TPL TAG impl
+/** TPL TAG impl
  *
- * @author Alexander I. Kharitchev
- */
+ * @author Alexander I. Kharitchev */
 final class TagCALL implements Instruction {
-	
+
 	private final String expression;
-	
+
 	private final ProgramPart call;
-	
+
 	private final ProgramPart source;
-	
+
 	TagCALL(final Function<String, String> folder, final Stack<TagRECURSION> recursions, final String expression, final Token[] source) throws Evaluate.CompilationException {
+		
 		this.expression = expression;
 		this.call = Evaluate.prepareFunctionObjectForExpression(expression, null);
 		this.source = TplParser.createProgramPart(folder, this, recursions, source);
 	}
-	
+
 	@Override
 	public final ExecStateCode execCall(final ExecProcess ctx) {
-		
+
 		final BaseFunction prev = Context.sourceReplace(ctx, this.source);
 		try {
 			ctx.vmFrameEntryExCall(true, null, this.call, ExecArgumentsEmpty.INSTANCE, ResultHandler.FA_BNN_NXT);
@@ -47,9 +46,7 @@ final class TagCALL implements Instruction {
 				return null;
 			}
 			return (ExecStateCode) result;
-		} catch (final Error e) {
-			throw e;
-		} catch (final ExecNonMaskedException e) {
+		} catch (final Error | ExecNonMaskedException e) {
 			throw e;
 		} catch (final Throwable t) {
 			throw new RuntimeException("CALL: " + this.expression + ", class=" + t.getClass().getName(), t);
@@ -57,22 +54,22 @@ final class TagCALL implements Instruction {
 			Context.sourceReplace(ctx, prev);
 		}
 	}
-	
+
 	@Override
 	public final int getOperandCount() {
-		
+
 		return 0;
 	}
-	
+
 	@Override
 	public final int getResultCount() {
-		
+
 		return 0;
 	}
-	
+
 	@Override
 	public String toCode() {
-		
+
 		return "CALL: " + this.expression;
 	}
 }
